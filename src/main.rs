4e69f334c90299858ef466821f8a77f554976c26
phil-bell <philhabell@gmail.com>
2023-12-31 12:42:57 +0000

    changing example code to be  json api instead of tera template

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {req:?}");
-    "Hello world!"
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+
+/// This handler uses json extractor
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    println!("model: {:?}", &item);
+    HttpResponse::Ok().json(item.0) // <- send response
+}
+
+/// This handler uses json extractor with limit
+async fn extract_item(item: web::Json<MyObj>, req: HttpRequest) -> HttpResponse {
+    println!("request: {req:?}");
+    println!("model: {item:?}");
+
+    HttpResponse::Ok().json(item.0) // <- send json response
+}
+
+/// This handler manually load request payload and parse json object
+async fn index_manual(body: web::Bytes) -> Result<HttpResponse, Error> {
+    // body is loaded, now we can deserialize serde-json
+    let obj = serde_json::from_slice::<MyObj>(&body)?;
+    Ok(HttpResponse::Ok().json(obj)) // <- send response
 }
 
 #[actix_web::main]
@@ -15,8 +38,15 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
+            .app_data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
+            .service(web::resource("/extractor").route(web::post().to(index)))
+            .service(
+                web::resource("/extractor2")
+                    .app_data(web::JsonConfig::default().limit(1024)) // <- limit size of the payload (resource level)
+                    .route(web::post().to(extract_item)),
+            )
+            .service(web::resource("/manual").route(web::post().to(index_manual)))
+            .service(web::resource("/").route(web::post().to(index)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -25,23 +55,28 @@ async fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use actix_web::{body::to_bytes, dev::Service, http, test, web, App, Error};
+    use actix_web::{body::to_bytes, dev::Service, http, test, web, App};
 
     use super::*;
 
     #[actix_web::test]
-    async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let app = test::init_service(app).await;
+    async fn test_index() {
+        let app =
+            test::init_service(App::new().service(web::resource("/").route(web::post().to(index))))
+                .await;
 
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await?;
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(MyObj {
+                name: "my-name".to_owned(),
+                number: 43,
+            })
+            .to_request();
+        let resp = app.call(req).await.unwrap();
 
         assert_eq!(resp.status(), http::StatusCode::OK);
 
-        let response_body = resp.into_body();
-        assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
-
-        Ok(())
+        let body_bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(body_bytes, r#"{"name":"my-name","number":43}"#);
     }
 }
